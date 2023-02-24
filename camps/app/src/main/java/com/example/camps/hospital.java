3390acbea6ce331cc417597a4d1dfae5f65966b4
saishock1504 <>
2023-02-24 17:15:51 +0530

    cardviewhospital

---
+++ camps/app/src/main/java/com/example/camps/hospital.java
@@ -0,0 +1,14 @@
+package com.example.camps;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital);
+    }
+}
